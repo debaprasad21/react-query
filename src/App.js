f9e8860f9cccda52ba4acb366ddeeb6d247aa5c7
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-03-10 23:37:08 +0530

    refetchInterval

--- src/App.js
+++ src/App.js
@@ -9,14 +9,10 @@ function App() {
       fetch("https://jsonplaceholder.typicode.com/posts").then((res) => {
         return res.json();
       }),
-    staleTime: 4000,
+    refetchInterval: 4000,
   });
-  /* StaleTime - 
-  if there is no change in query, it will refetch automatically in the background based on
-  i. a new instance of the query is mounted
-  ii. window is refocused
-  iii. Network is reconnected
-  iv. when the query is optionally configured such that it has refetch.
+  /* refetchInterval - 
+  Refetchs after certain interval
   */
 
   const { mutate, isPending, isError, isSuccess } = useMutation({
