992da68bbbf5709ed2658010df36f33e692eaaeb
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-03-10 23:04:16 +0530

    mutation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) => {
+        return res.json();
+      }),
+  });
+
+  const { mutate, isPending, isError, isSuccess } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: { "Content-type": "application/json" },
+      }).then((res) => {
+        return res.json();
+      }),
+  });
+
+  if (error || isError) return <div>There was an error</div>;
+
+  if (isLoading) return <div>Data is loading</div>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isPending && <p>Data is Being Added...</p>}
+      <button
+        onClick={() =>
+          mutate({
+            userId: 5000,
+            id: 4000,
+            title: "this is the title",
+            body: "this is the body",
+          })
+        }
+      >
+        Add Posts
+      </button>
+      {data &&
+        data.map((todo) => (
+          <div key={todo.id}>
+            <h4>id: {todo.id}</h4>
+            <h4>Title: {todo.title}</h4>
+            <p>{todo.body}</p>
+          </div>
+        ))}
     </div>
   );
 }
