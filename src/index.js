b552f2cbd8340385bb95c26b8c1507d010a83ac3
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-03-10 23:39:46 +0530

    default all useQueries with staleTime

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 6000 } },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
